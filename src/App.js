f945e345b769338fd9b2d3a4d8e6247932480b4a
Klajdi <klajdizmalaj@icloud.com>
2020-10-22 18:25:01 +0200

    m

--- src/App.js
+++ src/App.js
@@ -3,6 +3,81 @@ import "tui-image-editor/dist/tui-image-editor.css";
 import ImageEditor from "@toast-ui/react-image-editor";
 import "./App.css";
 
+var customTheme = {
+  "common.bi.image": "none",
+  "common.bisize.width": "0",
+  "common.bisize.height": "0",
+  "common.backgroundImage": "dope",
+  "common.backgroundColor": "#ffe7e6",
+  "common.border": "0px",
+
+  // header
+  "header.backgroundImage": "none",
+  "header.backgroundColor": "transparent",
+  "header.border": "0px",
+
+  // load button
+  "loadButton.backgroundColor": "#fff",
+  "loadButton.border": "1px solid #ddd",
+  "loadButton.color": "#222",
+  "loadButton.fontFamily": "NotoSans, sans-serif",
+  "loadButton.fontSize": "12px",
+
+  // download button
+  "downloadButton.backgroundColor": "#222",
+  "downloadButton.border": "1px solid #222",
+  "downloadButton.color": "#fff",
+  "downloadButton.fontFamily": "NotoSans, sans-serif",
+  "downloadButton.fontSize": "12px",
+
+  // icons default
+  "menu.normalIcon.color": "#8a8a8a",
+  "menu.activeIcon.color": "#555555",
+  "menu.disabledIcon.color": "#434343",
+  "menu.hoverIcon.color": "#ff87b0",
+  "submenu.normalIcon.color": "#8a8a8a",
+  "submenu.activeIcon.color": "#ff87b0",
+
+  "menu.iconSize.width": "24px",
+  "menu.iconSize.height": "24px",
+  "submenu.iconSize.width": "32px",
+  "submenu.iconSize.height": "32px",
+
+  // submenu primary color
+  "submenu.backgroundColor": "#ffdcdb",
+  "submenu.partition.color": "#222",
+
+  // submenu labels
+  "submenu.normalLabel.color": "#222",
+  "submenu.normalLabel.fontWeight": "lighter",
+  "submenu.activeLabel.color": "#222",
+  "submenu.activeLabel.fontWeight": "lighter",
+
+  // checkbox style
+  "checkbox.border": "1px solid #ff87b0",
+  "checkbox.backgroundColor": "#ff87b0",
+
+  // rango style
+  "range.pointer.color": "#fff",
+  "range.bar.color": "#666",
+  "range.subbar.color": "#d1d1d1",
+
+  "range.disabledPointer.color": "#414141",
+  "range.disabledBar.color": "#282828",
+  "range.disabledSubbar.color": "#414141",
+
+  "range.value.color": "#fff",
+  "range.value.fontWeight": "lighter",
+  "range.value.fontSize": "11px",
+  "range.value.border": "1px solid #353535",
+  "range.value.backgroundColor": "#151515",
+  "range.title.color": "#fff",
+  "range.title.fontWeight": "lighter",
+
+  // colorpicker style
+  "colorpicker.button.border": "1px solid #1e1e1e",
+  "colorpicker.title.color": "#fff",
+};
 class App extends React.Component {
   state = {
     hasImg: "",
@@ -58,7 +133,7 @@ class App extends React.Component {
                   path: hasImg,
                   name: "SampleImage",
                 },
-
+                theme: customTheme,
                 uiSize: {
                   width: "1000px",
                   height: "730px",
